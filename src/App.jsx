de50795a991cc98c83643b80c3389e42554cf767
wangsungtaek <tjdxorv@naver.com>
2024-04-09 23:42:15 +0900

    study State

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import ChildProperty from './03/ChildProperty';
+import StateExample from './03/StateExample';
+// import ChildProperty from './03/ChildProperty';
 // import DefaultPropsComponent from './03/DefaultPropsComponent';
 // import ChildComponent2 from './03/ChildComponent2';
 // import ChildComponent from './03/ChildComponent';
@@ -23,9 +24,10 @@ class App extends React.Component {
       // />
       // <ChildComponent2 objValue={{ name: 'Taek', age: 12 }} />
       // <DefaultPropsComponent />
-      <ChildProperty>
-        <div>이게 나온다고?</div>
-      </ChildProperty>
+      // <ChildProperty>
+      //   <div>이게 나온다고?</div>
+      // </ChildProperty>
+      <StateExample />
     );
   }
 }
